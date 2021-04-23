7703d040fd0e16ca1a9652ee19159834a84f77b0
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-04-23 04:37:59 -0700

    Stub out Input module

---
+++ app/scripts/Input.js
@@ -0,0 +1,27 @@
+export default class InputHandler {
+
+  /**
+   * @constructor
+   */
+  constructor(config)
+  {
+    document.addEventListener('keydown', this.handleKeyDnEvent)
+    document.addEventListener('keyup',   this.handkeKeyUpEvent)
+  }
+
+  handleKeyDnEvent(event)
+  {
+  }
+
+  handleKeyUpEvent(event)
+  {
+  }
+
+  handleMouseDnEvent(event)
+  {
+  }
+
+  handleMouseUpEvent(event)
+  {
+  }
+}
