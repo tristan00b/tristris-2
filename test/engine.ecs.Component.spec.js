ae5f73d5c663c28fccf7775a9a1c463feafc8693
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-06-21 16:51:15 -0700

    Update tests

---
+++ test/engine.ecs.Component.spec.js
@@ -0,0 +1,20 @@
+import { Component } from '../app/scripts/engine/ecs/components/Component'
+import { getPropertyDescriptors } from '../app/scripts/engine/utilities'
+
+describe('MakeComponent', function () {
+
+  class TestComponent extends Component
+  {
+    constructor(...args)
+    {
+      super(...args)
+    }
+  }
+
+  const testComponent = new TestComponent
+
+  it('declares itself to be a component type', function () {
+    expect(TestComponent.isComponent).toBe(true)
+    expect(testComponent.isComponent).toBe(true)
+  })
+})
