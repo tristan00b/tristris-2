a840fe3441e074c2c2c6e9c17d71f8634e84dcc5
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-06-21 14:16:55 -0700

    Add convenience exports for component modules

---
+++ app/scripts/engine/ecs/components/all.js
@@ -0,0 +1,4 @@
+/** @module Engine/ecs/Components */
+
+export { /** @see Component          */ Component          } from './Component'
+export { /** @see TagComponent       */ TagComponent       } from './TagComponent'
