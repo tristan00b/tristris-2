d01da41458ffda53a0747c95ba976fe39f85fd57
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-06-26 20:57:16 -0700

    Update entity component system exports

--- app/scripts/engine/ecs/all.js
+++ app/scripts/engine/ecs/all.js
@@ -2,4 +2,6 @@
 
 export { /** @see Component          */ Component } from './Component'
 export { /** @see Entity             */ Entity    } from './Entity'
+export { /** @see Query              */ Query     } from './Query'
+export { /** @see Scene              */ Scene     } from './Scene'
 export { /** @see System             */ System    } from './System'
