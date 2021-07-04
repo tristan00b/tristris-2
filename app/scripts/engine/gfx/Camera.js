b956fa2f3639223740bc7c61f91414dc0e62be56
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-07-04 16:02:24 -0700

    Refactor Camera

--- app/scripts/engine/gfx/Camera.js
+++ app/scripts/engine/gfx/Camera.js
@@ -1,5 +1,4 @@
 import { mat4, vec3 } from 'gl-matrix'
-
 import { MakeErrorType, MakeLogger } from '../utilities'
 
 
@@ -27,8 +26,13 @@ export class Camera
   /**
    * Sets the lookat tranform matrix
    * @type {Object<external:vec3,external:vec3,external:vec3>}
+   * @param {Object} args
+   * @param {external:vec3} [args.eye=[0,0,0]] The camera's position in world coordinates
+   * @param {external:vec3} [args.at=[0,0,0]]  The position that the camera is looking at in world coordinates
+   * @param {external:vec3} [args.up=[0,0,0]]  A vector specifying the direction that the top of the camera
+   *                                           points (often aligns with the y-axis)
    */
-  set lookat({ eye, at, up })
+  setLookat({ eye, at, up })
   {
     this._eye = eye ?? this._eye ?? [0, 0, 0]
     this._at  = at  ?? this._at  ?? [0, 0, 0]
@@ -38,6 +42,8 @@ export class Camera
       this._eye,
       this._at,
       this._up)
+
+    return this
   }
 
   /**
@@ -50,11 +56,20 @@ export class Camera
     return this._lookat
   }
 
-  set perspective({ near, far, fovy, aspect })
+  /**
+   * Sets the perspective projection matrix
+   * @param {Object} args
+   * @param {Number} [args.near=0.1]
+   * @param {Number} [args.far=100]
+   * @param {Number} [args.fovy=Math.PI/4]
+   * @param {Number} [args.aspect=300/150]
+   */
+  setPerspective({ near, far, fovy, aspect })
   {
+    // Set properties by priority: arg < current value < default value
     this._near   = near   ?? this._near   ?? 0.1,
     this._far    = far    ?? this._far    ?? 100.0,
-    this._fovy   = fovy   ?? this._fovy   ?? 45.0 * Math.PI / 180.0,
+    this._fovy   = fovy   ?? this._fovy   ?? Math.PI / 4.0,
     this._aspect = aspect ?? this._aspect ?? 300/150
 
     this._perspective = mat4.perspective(mat4.create(),
@@ -62,15 +77,8 @@ export class Camera
       this._aspect,
       this._near,
       this._far)
-  }
 
-  /**
-   * @type {external:mat4}
-   * @readonly
-   */
-  get projection()
-  {
-    return this._perspective
+    return this
   }
 
   /**
@@ -81,6 +89,24 @@ export class Camera
   setAspect({ width, height }) // ignore coverage
   {
     this.perspective = { aspect: width/height }
+  }
+
+  /**
+   * @type {external:mat4}
+   * @readonly
+   */
+  get perspective()
+  {
+    return this._perspective
+  }
+
+  /**
+   * @type {external:mat4}
+   * @readonly
+   */
+  get projection()
+  {
+    return this._perspective
   }
 }
 
