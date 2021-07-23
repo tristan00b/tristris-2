1c3226d37f73adad09b0ea55561b9d96c42d70d0
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-07-22 22:53:04 -0700

    Include alpha component in materials

--- app/scripts/engine/gfx/Material.js
+++ app/scripts/engine/gfx/Material.js
@@ -1,4 +1,4 @@
-import { vec3 } from 'gl-matrix'
+import { vec4 } from 'gl-matrix'
 
 /**
  * Basic Material class for storing mesh colour properties
@@ -12,16 +12,16 @@ export class Material
   /**
    * @todo add constraint checking
    * @param {Object} [args]
-   * @param {vec3} [args.ambient=[0,0,0]] The ambient light reflectivity
-   * @param {vec3} [args.diffuse=[1,1,1]] The diffuse light reflectivity
-   * @param {vec3} [args.specular=[0,0,0]] The specular light reflectivity
+   * @param {vec4} [args.ambient=[0,0,0,1]] The ambient light reflectivity
+   * @param {vec4} [args.diffuse=[1,1,1,1]] The diffuse light reflectivity
+   * @param {vec4} [args.specular=[0,0,0,1]] The specular light reflectivity
    * @param {number} [args.shininess=32] The shininess coefficient
    */
   constructor({ ambient, diffuse, specular, shininess } = {})
   {
-    this.ambient   = ambient   ?? vec3.create(0, 0, 0)
-    this.diffuse   = diffuse   ?? vec3.create(1, 1, 1)
-    this.specular  = specular  ?? vec3.create(0, 0, 0)
+    this.ambient   = ambient   ?? vec4.create(0, 0, 0, 1)
+    this.diffuse   = diffuse   ?? vec4.create(1, 1, 1, 1)
+    this.specular  = specular  ?? vec4.create(0, 0, 0, 1)
     this.shininess = shininess ?? 32
   }
 }
