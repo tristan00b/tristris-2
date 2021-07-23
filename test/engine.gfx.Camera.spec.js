39a50e2b4d683464d5a74d0bda7653400b432b2e
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-07-22 22:57:18 -0700

    Update Camera tests

--- test/engine.gfx.Camera.spec.js
+++ test/engine.gfx.Camera.spec.js
@@ -2,11 +2,11 @@ import { Camera } from '../app/scripts/engine/gfx/Camera'
 import { mat4 } from 'gl-matrix'
 
 
-describe('Camera', function () {
+describe('Camera', () => {
 
   const identity = mat4.create()
 
-  describe('Camera#lookat', function () {
+  describe('Camera#lookat', () => {
 
     const eye = [0, -1,  0]
     const up  = [0,  1,  0]
@@ -14,20 +14,20 @@ describe('Camera', function () {
     const lookat = mat4.lookAt(mat4.create(), eye, at, up)
 
 
-    it('default constructs an identity matrix', function () {
+    it('default constructs an identity matrix', () => {
       const cam = new Camera
       expect( cam.lookat.toString() ).toEqual( identity.toString() )
     })
 
 
-    it('can generate a lookat matrix', function () {
+    it('can generate a lookat matrix', () => {
       const cam = new Camera
       cam.setLookat({ eye, at, up })
       expect( cam.lookat.toString() ).toEqual( lookat.toString() )
     })
 
 
-    it('overwrites a lookat parameter when after already being set', function () {
+    it('overwrites a lookat parameter when after already being set', () => {
       const cam = new Camera
       cam.setLookat({ eye: [1,2,3] })
       cam.setLookat({ eye: [3,2,1] })
@@ -35,7 +35,7 @@ describe('Camera', function () {
     })
 
 
-    it('remembers previously set values when another is modified', function () {
+    it('remembers previously set values when another is modified', () => {
       const cam = new Camera
       cam.setLookat({ eye })
       cam.setLookat({ at  })
@@ -44,35 +44,35 @@ describe('Camera', function () {
   })
 
 
-  describe('Camera.perspective', function () {
+  describe('Camera.perspective', () => {
 
     const near   = 0.1
     const far    = 100
-    const fovy   = Math.PI / 4
+    const fovy   = Math.PI / 2
     const aspect = 1.5
     const perspective = mat4.perspective(mat4.create(), fovy, aspect, near, far)
 
 
-    it('default constructs a perspective matrix', function () {
+    it('default constructs a perspective matrix', () => {
       const cam = new Camera
       expect( cam.projection.toString() ).toEqual( perspective.toString() )
     })
 
 
-    it('cat generate a perspective matrix', function () {
+    it('cat generate a perspective matrix', () => {
       const cam = new Camera
       cam.setPerspective({ near, far, fovy, aspect })
       expect( cam.projection.toString() ).toEqual( perspective.toString() )
     })
 
-    it('overwrites a perspective parameter when after already being set', function () {
+    it('overwrites a perspective parameter when after already being set', () => {
       const cam = new Camera
       cam.setPerspective({ near: 11 })
       cam.setPerspective({ near: 22 })
       expect( cam._near.toString() ).toEqual( (22).toString() )
     })
 
-    it('remembers previously set values when another is set', function () {
+    it('remembers previously set values when another is set', () => {
       const cam = new Camera
       cam.setPerspective({ near })
       cam.setPerspective({ far  })
