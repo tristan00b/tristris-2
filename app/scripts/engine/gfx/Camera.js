33e100bead4b9fff3c35de42b5ee11776f88f6b7
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-07-26 01:28:20 -0700

    Lazily compute matrices

--- app/scripts/engine/gfx/Camera.js
+++ app/scripts/engine/gfx/Camera.js
@@ -2,24 +2,14 @@ import { mat4, vec3 } from 'gl-matrix'
 
 
 /**
- * Describes the view point from which to draw a scene
+ * Generates 4x4 lookat and perspective matrices
  */
 export class Camera
 {
   constructor()
   {
-    this.setLookat({
-      eye: [0, 0, 0],
-      at:  [0, 0, 0],
-      up:  [0, 0, 0],
-     })
-
-     this.setPerspective({
-      near:   0.1,
-      far:    100.0,
-      fovy:   Math.PI / 2,
-      aspect: 1.5,
-     })
+    this.setLookat()
+    this.setPerspective()
   }
 
   // -------------------------------------------------------------------------------------------------------------------
@@ -27,22 +17,19 @@ export class Camera
   //
 
   /**
-   * The location of the camera (setting triggers a matrix calculation; use {@link Camera#setLookat} to avoid triggering
-   * extra calculations by setting properties individually)
+   * The location of the camera
    * @type {external:vec3}
-   * @default [0,0,0]
+   * @default [0,0,1]
    */
   get eye() { return this._eye }
 
   set eye(eye)
   {
-    if (this._eye !== eye)
-      this.setLookat({ eye })
+    this.setLookat({ eye })
   }
 
   /**
-   * The location that the camera is looking at (setting triggers a matrix calculation; use {@link Camera#setLookat} to
-   * avoid triggering extra calculations by setting properties individually)
+   * The location that the camera is looking at
    * @type {external:vec3}
    * @default [0,0,0]
    */
@@ -50,22 +37,19 @@ export class Camera
 
   set at(at)
   {
-    if (this._at !== at)
-      this.setLookat({ at })
+    this.setLookat({ at })
   }
 
   /**
-   * The camera's up vector (setting triggers a matrix calculation; use {@link Camera#setLookat} to avoid triggering
-   * extra calculations by setting properties individually)
+   * The camera's up vector
    * @type {external:vec3}
-   * @default [0,0,0]
+   * @default [0,1,0]
    */
   get up() { return this._up }
 
   set up(up)
   {
-    if (this._up !== up)
-      this.setLookat({ up })
+    this.setLookat({ up })
   }
 
   /**
@@ -75,30 +59,32 @@ export class Camera
    */
   get lookat()
   {
+    if (this._lookatIsDirty)
+    {
+      this._lookat = computeLookatMatrix(this)
+      this._lookatIsDirty = false
+    }
+
     return this._lookat
   }
 
   /**
-   * Sets the lookat tranform matrix (triggers a matrix calculation)
+   * Sets the lookat tranform matrix
    * @type {Object<external:vec3,external:vec3,external:vec3>}
    * @param {Object} args
-   * @param {external:vec3} [args.eye=[0,0,0]] The camera's position in world coordinates
+   * @param {external:vec3} [args.eye=[0,0,1]] The camera's position in world coordinates
    * @param {external:vec3} [args.at=[0,0,0]]  The position that the camera is looking at in world coordinates
-   * @param {external:vec3} [args.up=[0,0,0]]  A vector specifying the direction that the top of the camera
+   * @param {external:vec3} [args.up=[0,1,0]]  A vector specifying the direction that the top of the camera
    *                                           points (often aligns with the y-axis)
    * @returns {Camera} The `this` object reference
    */
-  setLookat({ eye, at, up })
+  setLookat({ eye, at, up } = {})
   {
-    this._eye = eye ?? this._eye ?? [0, 0, 0]
+    // Set properties by priority: arg > currently set value > default value
+    this._lookatIsDirty = true
+    this._eye = eye ?? this._eye ?? [0, 0, 1]
     this._at  = at  ?? this._at  ?? [0, 0, 0]
-    this._up  = up  ?? this._up  ?? [0, 0, 0]
-
-    this._lookat = mat4.lookAt(mat4.create(),
-      this._eye,
-      this._at,
-      this._up)
-
+    this._up  = up  ?? this._up  ?? [0, 1, 0]
     return this
   }
 
@@ -116,8 +102,7 @@ export class Camera
 
   set near(near)
   {
-    if (this._near !== near)
-      this.setPerspective({ near })
+    this.setPerspective({ near })
   }
 
   /**
@@ -130,22 +115,20 @@ export class Camera
 
   set far(far)
   {
-    if (this._far !== far)
-      this.setPerspective({ far })
+    this.setPerspective({ far })
   }
 
   /**
    * The camera's y-axis field of view (setting triggers a matrix calculation; use {@link Camera#setPerspective} to
    * avoid triggering extra calculations by setting properties individually)
    * @type {Number}
-   * @default Math.PI/4
+   * @default (60*Math.PI/180)
    */
   get fovy() { return this._fovy }
 
   set fovy(fovy)
   {
-    if (this._fovy !== fovy)
-      this.setPerspective({ fovy })
+    this.setPerspective({ fovy })
   }
 
   /**
@@ -158,8 +141,7 @@ export class Camera
 
   set aspect(aspect)
   {
-    if (this._aspect !== aspect)
-      this.setPerspective({ aspect })
+    this.setPerspective({ aspect })
   }
 
   /**
@@ -169,18 +151,13 @@ export class Camera
    */
   get perspective()
   {
-    return this._perspective
-  }
+    if (this._perspectiveIsDirty)
+    {
+      this._perspective = computePerspectiveMatrix(this)
+      this._perspectiveIsDirty = false
+    }
 
-  /**
-   * Aliases `perspective` property
-   * @type {external:mat4}
-   * @see {@link Camera#perspective}
-   * @readonly
-   */
-  get projection()
-  {
-    return this.perspective
+    return this._perspective
   }
 
   /**
@@ -188,34 +165,32 @@ export class Camera
    * @param {Object} args
    * @param {Number} [args.near=0.1] the near plain distance
    * @param {Number} [args.far=100] The far plane distance
-   * @param {Number} [args.fovy=Math.PI/4] The field of view (45 degrees)
+   * @param {Number} [args.fovy=(60 * Math.PI/180)] The y-axis field of view (radians)
    * @param {Number} [args.aspect=1.5] The aspect ratio (width/height)
    * @returns {Camera} The `this` object reference
    */
-  setPerspective({ near, far, fovy, aspect })
+  setPerspective({ near, far, fovy, aspect } = {})
   {
-    // Set properties by priority: arg > current value > default value
+    // Set properties by priority: arg > currently set value > default value
+    this._perspectiveIsDirty = true
     this._near   = near   ?? this._near   ?? 0.1,
     this._far    = far    ?? this._far    ?? 100.0,
-    this._fovy   = fovy   ?? this._fovy   ?? Math.PI / 4.0,
+    this._fovy   = fovy   ?? this._fovy   ?? 60 * Math.PI/180,
     this._aspect = aspect ?? this._aspect ?? 1.5
-
-    this._perspective = mat4.perspective(mat4.create(),
-      this._fovy,
-      this._aspect,
-      this._near,
-      this._far)
-
     return this
   }
+}
 
-  /**
-   * Aliases `setPerspective` property
-   * @param {Object} args
-   * @see {@link Camera#setPerspective})
-   */
-  setProjection(args)
-  {
-    return this.setPerspective(args)
-  }
+
+/** @private */
+function computeLookatMatrix({ eye, at, up })
+{
+  return mat4.lookAt([], eye, at, up)
+}
+
+
+/** @private */
+function computePerspectiveMatrix({ fovy, aspect, near, far})
+{
+  return mat4.perspective([], fovy, aspect, near, far)
 }
--- test/engine.gfx.Camera.spec.js
+++ test/engine.gfx.Camera.spec.js
@@ -1,45 +1,37 @@
 import { Camera } from '../app/scripts/engine/gfx/Camera'
-import { mat4 } from 'gl-matrix'
+import { mat4,
+         vec3   } from 'gl-matrix'
 
 
 describe('Camera', () => {
 
   const identity = mat4.create()
 
-  describe('Camera#lookat', () => {
+  describe('Camera.lookat', () => {
 
     const eye = [0, -1,  0]
     const up  = [0,  1,  0]
     const at  = [0,  0,  0]
     const lookat = mat4.lookAt(mat4.create(), eye, at, up)
 
-
-    it('default constructs an identity matrix', () => {
-      const cam = new Camera
-      expect( cam.lookat.toString() ).toEqual( identity.toString() )
-    })
-
-
     it('can generate a lookat matrix', () => {
       const cam = new Camera
       cam.setLookat({ eye, at, up })
-      expect( cam.lookat.toString() ).toEqual( lookat.toString() )
+      expect(mat4.equals(cam.lookat, lookat)).toBe(true)
     })
-
 
     it('overwrites a lookat parameter when after already being set', () => {
       const cam = new Camera
       cam.setLookat({ eye: [1,2,3] })
       cam.setLookat({ eye: [3,2,1] })
-      expect( cam._eye.toString() ).toEqual( [3,2,1].toString() )
+      expect(vec3.equals(cam.eye, [3,2,1])).toBe(true)
     })
-
 
     it('remembers previously set values when another is modified', () => {
       const cam = new Camera
       cam.setLookat({ eye })
       cam.setLookat({ at  })
-      expect( cam._at.toString() ).toEqual( at.toString() )
+      expect(vec3.equals(cam.at, at)).toBe(true)
     })
   })
 
@@ -52,31 +44,24 @@ describe('Camera', () => {
     const aspect = 1.5
     const perspective = mat4.perspective(mat4.create(), fovy, aspect, near, far)
 
-
-    it('default constructs a perspective matrix', () => {
-      const cam = new Camera
-      expect( cam.projection.toString() ).toEqual( perspective.toString() )
-    })
-
-
     it('cat generate a perspective matrix', () => {
       const cam = new Camera
       cam.setPerspective({ near, far, fovy, aspect })
-      expect( cam.projection.toString() ).toEqual( perspective.toString() )
+      expect(mat4.equals(cam.perspective, perspective)).toBe(true)
     })
 
     it('overwrites a perspective parameter when after already being set', () => {
       const cam = new Camera
       cam.setPerspective({ near: 11 })
       cam.setPerspective({ near: 22 })
-      expect( cam._near.toString() ).toEqual( (22).toString() )
+      expect(cam.near).toBe(22)
     })
 
     it('remembers previously set values when another is set', () => {
       const cam = new Camera
       cam.setPerspective({ near })
       cam.setPerspective({ far  })
-      expect(cam._near).toEqual(near)
+      expect(cam.near).toBe(near)
     })
   })
 })
