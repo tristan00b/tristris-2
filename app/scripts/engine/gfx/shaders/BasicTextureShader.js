f761f047d3dd29cd3cf428c6c5bf98756012811a
Tristan Bayfield <tristan.bayfield@gmail.com>
2021-09-13 17:43:23 -0700

    Implement Blinn-Phong shading

--- app/scripts/engine/gfx/shaders/BasicTextureShader.js
+++ app/scripts/engine/gfx/shaders/BasicTextureShader.js
@@ -11,8 +11,6 @@ export class BasicTextureShader extends ShaderProgram
   {
     lightCount ??= 1
 
-    // console.log(`BasicTextureShader.lightCount: ${lightCount}`)
-
     super(gl,
       {
         type:     gl.VERTEX_SHADER,
@@ -95,10 +93,10 @@ export class BasicTextureShader extends ShaderProgram
                       vec3 light_pos = vec3(m.view * vec4(lights[i].position, 1.0));
 
                       vec3 L = normalize(light_pos - pass_vertex_position);
-                      vec3 R = reflect(-L, N);
+                      vec3 H = normalize(L + V);
 
                       float kd = max(dot(N, L), 0.0);
-                      float ks = pow(max(dot(V, R), 0.0), material.shininess);
+                      float ks = pow(max(dot(N, H), 0.0), material.shininess);
 
                       vec3 ambient  =       lights[i].colour * material.ambient;
                       vec3 diffuse  = kd  * lights[i].colour * texture(sampler, UV).xyz;
--- app/scripts/game/scenes/Textures.js
+++ app/scripts/game/scenes/Textures.js
@@ -131,7 +131,7 @@ export function MakeScene(gl)
       ambient:  [0.0, 0.0, 0.0],
       diffuse:  [1.0, 1.0, 1.0],
       specular: [0.7, 0.7, 0.7],
-      shininess: 5,
+      shininess: 6,
     }
 
     const m = new Material
@@ -181,7 +181,7 @@ export function MakeScene(gl)
       ambient:  [0.0, 0.0, 0.0],
       diffuse:  [0.9, 0.9, 0.9],
       specular: [0.7, 0.7, 0.7],
-      shininess: 64,
+      shininess: 128,
     }
 
     const m = new Material
